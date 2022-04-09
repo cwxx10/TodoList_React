2ed573e4a492131825df29da6e42a98e058774e5
Ciro Matheus <cmatheuzz@gmail.com>
2022-04-09 17:10:44 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [currentTask, setCurrentTask] = useState("");
+  const [qtdTask, setQtdTask] = useState(0);
+  const [style, setStyle] = useState(false);
+
+  const addTask = () => {
+    setTodoList([...todoList, currentTask]);
+    setQtdTask(qtdTask + 1);
+    //...todoList = tudo que já havia no array todoList
+    //currentTask é para adicionar no array a currenttask
+  };
+
+  const cleanTask = () => {
+    setTodoList([]);
+    setQtdTask(qtdTask === 0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>
+        <i id="ico1" class="fa-solid fa-circle-check"></i> Todo List
+      </h1>
+      <p className="p_qtd">Tasks: {qtdTask}</p>
+      <div>
+        <input
+          type="text"
+          placeholder="Type your task here"
+          onKeyDown={(event) => {
+            if (event.keyCode === 13) addTask();
+          }}
+          //onkeydown = function to enter
+          onChange={(event) => {
+            setCurrentTask(event.target.value);
+          }}
+        />
+        <button type="button" onClick={addTask}>
+          Add Task
+        </button>
+        <button onClick={cleanTask}>Clear All Tasks</button>
+        <hr />
+
+        <div>
+          {todoList.map((val, key) => {
+            return (
+              <ul>
+                <li>{val}</li>
+              </ul>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
